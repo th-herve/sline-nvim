54badebd523931c3f42afeafa7f6c24c0ac5d8e5
th-herve <thibault.herve1@gmail.com>
2024-06-07 14:11:32 +0200

    Fix leading '>' on bar when max sized

--- lua/sline/bar.lua
+++ lua/sline/bar.lua
@@ -14,7 +14,8 @@ local function get_breadcrum(depth)
     local extension = vim.fn.expand('%:e')
 
     local directories = vim.fn.expand('%:p:h')
-    local dirs_list = vim.split(directories, '/') -- break path into all directories
+    directories = directories:sub(2) -- remove leading '/'
+    local dirs_list = vim.split(directories, '/')
 
     local max = #dirs_list
     local min = #dirs_list - depth + 1
@@ -32,36 +33,21 @@ local function get_breadcrum(depth)
         end
     end
 
-    local icon, icon_hl =
-        devicon.get_icon(filename, extension, { default = true })
+    local icon, icon_hl = devicon.get_icon(filename, extension, { default = true })
 
-    return dir_bar
-        .. '%#'
-        .. icon_hl
-        .. '#'
-        .. icon
-        .. win_hl
-        .. ' '
-        .. filename
+    return dir_bar .. '%#' .. icon_hl .. '#' .. icon .. win_hl .. ' ' .. filename
 end
 
 ---@return bar_element
 local function get_diagnostics()
     local count = require('sline.diagnostics').get_count()
 
-    return '%#DiagnosticError#'
-        .. ' '
-        .. count.error
-        .. ' '
-        .. '%#DiagnosticWarn#'
-        .. ' '
-        .. count.warning
+    return '%#DiagnosticError#' .. ' ' .. count.error .. ' ' .. '%#DiagnosticWarn#' .. ' ' .. count.warning
 end
 
 ---@return bar_element
 local function get_git_branch()
-    local branch =
-        vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+    local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
 
     if branch ~= '' then
         branch = '%#Directory#  ' .. win_hl .. branch
@@ -72,11 +58,7 @@ end
 ---@param opts Config
 ---@return bar_element
 M.get_winbar = function(opts)
-    return get_breadcrum(opts.depth)
-        .. '%='
-        .. get_diagnostics()
-        .. '   '
-        .. get_git_branch()
+    return get_breadcrum(opts.depth) .. '%=' .. get_diagnostics() .. '   ' .. get_git_branch()
 end
 
 return M
