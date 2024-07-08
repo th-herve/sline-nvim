dfebac93d23a1f52c4667fbf18228515a18b2300
th-herve <thibault.herve1@gmail.com>
2024-07-08 21:52:11 +0200

    Update test configs

--- test/conf2.lua
+++ test/conf2.lua
@@ -12,16 +12,16 @@
 -- |     Lazy.nvim installation    |
 -- +-------------------------------+
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -29,21 +29,22 @@ vim.opt.rtp:prepend(lazypath)
 -- |        Plugins config         |
 -- +-------------------------------+
 
-require("lazy").setup({
-  {
-    dir = "~/programming/project/sline-nvim/",   -- Your path
-    name = "sline",
-    dependencies = { "nvim-tree/nvim-web-devicons", "tpope/vim-fugitive" },
-    opts = {
-      depth = 300,
-      status_line = false,
-    }
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
+require('lazy').setup({
+    {
+        dir = '~/programming/project/sline-nvim/', -- Your path
+        name = 'sline',
+        dependencies = { 'nvim-tree/nvim-web-devicons', 'tpope/vim-fugitive' },
+        opts = {
+            depth = 1,
+            status_line = false,
+            bg_color = 'none',
+        },
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+    },
 })
 
-require("telescope").setup()
+require('telescope').setup()
